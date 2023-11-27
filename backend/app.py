91382af6d35dc53e7e3a972988bfe4ff9f6d1a78
EdisonnTran <tran.edison52@gmail.com>
2023-11-26 20:21:43 -0500

    Create SQLAlchemy database

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 app = Flask(__name__, template_folder="../frontend")
 
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+db.init_app(app)
+
+from models import Item
+
 @app.route('/', methods=['GET', 'POST'])
 def tracker():
+    if (request.method == 'POST'):
+        data = request.form.get("itemName")
+        print(data)
     return render_template('tracker.html')
 
 
